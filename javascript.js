b560cc6dbeec4d1c02cb29060f65c2d29c61895d
student <mainagracewacheke@gmail.com>
2021-03-22 08:09:56 +0300

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function getGrade(marks){
+var marks=12;
+
+if (marks>=80){
+    console.log("A");
+}
+
+    else if (marks>=70){
+    console.log("B");
+    }
+
+    else if (marks>=60){
+        console.log("c");
+    }
+
+    else if (marks>=50){
+        console.log("D");
+    }
+
+    else if (marks>=49){
+        console.log("E");
+    }
+    else(marks<=0);{
+    console.log ("F");
+    }
+}
+getGrade()
